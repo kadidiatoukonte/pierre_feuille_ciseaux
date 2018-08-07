e496436814ef47b07d8061ed0d699861009782f0
kadidiatou <kadidiatou.konte@gmail.com>
2018-08-07 16:50:15 +0200

    correction

--- js/main.js
+++ js/main.js
@@ -1,31 +1,60 @@
-var choixUtilisateur = prompt("Choisissez-vous pierre, feuille, ou ciseaux ?");
-var choixOrdi = Math.floor(Math.random() * 3);
+var scoreUser = 0;
+var scoreOrdi = 0;
 
-while(choixUtilisateur != choixOrdi){
-if (choixOrdi === 0) {
-console.log("pierre");
+while(scoreOrdi<3 && scoreUser < 3){
+
+  var choixUser = prompt("Choisissez-vous pierre, feuille, ou ciseaux ?").toLowerCase();
+
+  if (choixUser != "pierre" && choixUser != "feuille" && choixUser != "ciseaux"){
+    console.log("Merci de choisir entre: une pierre, une feuille ou un ciseaux" );
+  }
+  else {
+    var choixOrdi = Math.floor(Math.random() * 3);
+
+    if (choixOrdi === 0) {
+      choixOrdi = "pierre";
+    } else if (choixOrdi === 1) {
+      choixOrdi = "ciseaux";
+    } else {
+      choixOrdi = "feuille";
+    }
+
+    if(choixOrdi === choixUser){
+      console.log("Égalité");
+    }else if (choixUser === "pierre"){
+      if (choixOrdi === "feuille") {
+        console.log("L'ordi a gagné");
+        scoreOrdi++;
+      }else {
+        console.log("Vous avez gagné");
+        scoreUser++;
+      }
+    }else if (choixUser === "feuille"){
+      if (choixOrdi === "ciseaux") {
+        console.log("L'ordi a gagné");
+        scoreOrdi++;
+      }else {
+        console.log("Vous avez gagné");
+        scoreUser++;
+      }
+    }else {
+      if (choixOrdi === "pierre") {
+        console.log("L'ordi a gagné");
+        scoreOrdi++;
+      }else {
+        console.log("Vous avez gagné");
+        scoreUser++;
+      }
+    }
+
+
+  }
+
 }
 
-else if (choixOrdi <= 0.33) {
-console.log("pierre");
+if (scoreUser === 3) {
+  console.log("Bravo vous emportez le match");
 }
-
-else if (choixOrdi === 0.34) {
-console.log("feuille");
-}
-
-else if (choixOrdi <= 0.66) {
-console.log("feuille");
-}
-
-else if (choixOrdi === 0.67) {
-console.log("ciseaux");
-}
-
 else {
-console.log("ciseaux");
+  console.log("Dommage l'ordi est le vainceur");
 }
-  console.log("Réessayé");
-  var choixUtilisateur = prompt("Choisissez-vous pierre, feuille, ou ciseaux ?");
-}
-console.log("Égalité");
